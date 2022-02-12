4f59959adaf2aad850921c93e6b4f9ecaf6a7111
KarasovRuslan <karasov.ruslan@icloud.com>
2022-02-12 19:00:09 +0200

    HW26

---
+++ src/info/data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+      "id": 1,
+      "firstName": 'Ruslan',
+      "lastName": 'Karasov',
+      "workdays": 15,
+      "rate": 20
+  },
+  {
+      "id": 2,
+      "firstName": 'Valery',
+      "lastName": 'Shap',
+      "workdays": 20,
+      "rate": 100
+  },
+  {
+      "id": 3,
+      "firstName": 'Kirill',
+      "lastName": 'Slivka',
+      "workdays": 27,
+      "rate": 200
+  }
+]
+
+export default data;
